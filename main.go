27f27abe8872b709f96a229b957ce331cd58357d
Temiloluwa Adebowale <65780579+stemitom@users.noreply.github.com>
2024-03-12 15:25:08 +0100

    latest changes

--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"path/filepath"
 	"sync"
 
 	"repo-pack/gh"
 	"repo-pack/helpers"
-
-	"github.com/cheggaaa/pb/v3"
 )
 
 func main() {
@@ -26,8 +23,8 @@ func run() error {
 	flag.Parse()
 
 	if *repoURL == "" {
-		flag.Usage()
-		return flag.ErrHelp
+		err := fmt.Errorf("missing argument for repoURL")
+		return err
 	}
 
 	components, err := helpers.ParseRepoURL(*repoURL)
@@ -43,15 +40,10 @@ func run() error {
 		return fmt.Errorf("failed to get files via contents API: %v", err)
 	}
 
-	baseDir := filepath.Base(components.Dir)
 	fmt.Printf("[-] Repository: %s/%s\n", components.Owner, components.Repository)
 	fmt.Printf("[-] GitHub Directory: %s\n", components.Dir)
 	fmt.Printf("[-] Fetching %d files\n", len(files))
 
-	bar := pb.Full.Start64(0)
-	defer bar.Finish()
-
-	var totalDownloaded int64
 	var wg sync.WaitGroup
 	errorsCh := make(chan error, len(files))
 
@@ -60,16 +52,9 @@ func run() error {
 		go func(file string) {
 			defer wg.Done()
 
-			content, err := gh.FetchPublicFile(ctx, file, &components)
+			err := gh.FetchPublicFile(ctx, file, &components)
 			if err != nil {
 				errorsCh <- fmt.Errorf("error fetching %s: %v", file, err)
-				return
-			}
-
-			totalDownloaded += int64(len(content))
-			bar.SetTotal(totalDownloaded)
-			if err := helpers.SaveFile(baseDir, file, content); err != nil {
-				errorsCh <- err
 				return
 			}
 		}(file)
